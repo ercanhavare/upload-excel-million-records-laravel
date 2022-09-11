42428fe9cbdb35de776e074a4afc5e1e005c7723
ercanhavare <ercanhavare@gmail.com>
2022-09-11 20:36:36 +0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\SalesController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get("/upload", [SalesController::class, "index"]);
+Route::post("/upload", [SalesController::class, "upload"]);
+Route::get("/batch", [SalesController::class, "batch"]);
+Route::get("/batch/in-progress", [SalesController::class, "batchInProgress"]);
